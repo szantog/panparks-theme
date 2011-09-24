3b6cba0b19271752432bc4792f6504f07a31816b
Gabor Szanto <hello@szantogabor.com>
2011-09-24 20:09:26 +0200

    Style site-closure

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -195,7 +195,7 @@
 </div></div><!-- /#page, /#page-wrapper -->
 
     <?php if ($page['footer']): ?>
-      <div id="footer-wrapper"><div id="footer"><div class="section clearfix">
+      <div id="footer-wrapper"><div id="footer"><div class="section clearfix">iá
         <?php print render($page['footer']); ?>
       </div></div></div>
     <?php endif; ?>
@@ -204,8 +204,8 @@
     <div id="site-closure-wrapper"><div id="site-closure"><div class="section clearfix">
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="small-logo"><img src="<?php print $small_logo_path; ?>" alt="<?php print t('Home'); ?>" /></a>
         <p>
-          <span class="uppercase title">The best of Europe's wilderness</span>
-          <big>network of national parks offering Europe's most undisturbed areas of nature, wild nature's holidays.</big>
+          <span class="uppercase title">The best of Europe's wilderness</span></br>
+          <span>network of national parks offering Europe's most undisturbed areas of nature, wild nature's holidays.</span>
         </p>
         <?php print render($page['site_closure']); ?>
         <div id ="macroweb"><small>designed by <a href="http://macroweb.hu">Macroweb</a>, powered by <a href="http://drupal.org">Drupal</a>.</small></div>
