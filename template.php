894796855998e708851ab3677e2b0066583f1811
Gabor Szanto <hello@szantogabor.com>
2011-10-13 13:08:25 +0200

    Fix anchor links in park pages

--- template.php
+++ template.php
@@ -193,6 +193,7 @@ function panparks_preprocess_page(&$vars, $hook) {
       array_unshift($vars['page']['site_closure']['menu_menu-site-closure'], $tmp);
     }
   }
+
   //kpr(get_defined_vars());
 
 /*
