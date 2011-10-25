55c94386e45bf919dccdcb710adcd95a723861ce
Gabor Szanto <hello@szantogabor.com>
2011-10-25 05:44:23 +0200

    No more need colorbox formatter overrides

--- template.php
+++ template.php
@@ -591,6 +591,7 @@ EOHTML;
 
 /**
  * Theme the field with pdf reader
+ * Need this to add link to original file under pdf view
  */
 function panparks_pdf_reader($variables) {
   switch ($variables['settings']['renderer']) {
@@ -627,113 +628,4 @@ function panparks_pdf_reader($variables) {
   }
   $output .= theme('file_link', array('file' => (object) $variables['file']));
   return $output;
-}
-
-/**
- * Overriden theme_colorbox_image_formatter
- * We need this, because of when using shared photos width colorbox, need to load the complete node, not only image
- * Returns HTML for an Colorbox image field formatter.
- *
- * @param $variables
- *   An associative array containing:
- *   - item: An array of image data.
- *   - image_style: An optional image style.
- *   - path: An array containing the link 'path' and link 'options'.
- *
- * @ingroup themeable
- */
-function panparks_colorbox_image_formatter($variables) {
-  $item = $variables['item'];
-  $node = $variables['node'];
-  $field = $variables['field'];
-  $settings = $variables['display_settings'];
-
-  $image = array(
-    'path' => $item['uri'],
-    'alt' => $item['alt'],
-    'title' => $item['title'],
-    'style_name' => $settings['colorbox_node_style'],
-  );
-
-  if (isset($item['width']) && isset($item['height'])) {
-    $image['width'] = $item['width'];
-    $image['height'] = $item['height'];
-  }
-
-  switch ($settings['colorbox_caption']) {
-     case 'auto':
-      // If the title is empty use alt or the node title in that order.
-      if (!empty($image['title'])) {
-        $caption = $image['title'];
-      }
-      elseif (!empty($image['alt'])) {
-        $caption = $image['alt'];
-      }
-      elseif (!empty($node->title)) {
-        $caption = $node->title;
-      }
-      else {
-        $caption = '';
-      }
-      break;
-    case 'title':
-      $caption = $image['title'];
-      break;
-    case 'alt':
-      $caption = $image['alt'];
-      break;
-    case 'node_title':
-      $caption = $node->title;
-      break;
-    case 'custom':
-      $caption = token_replace($settings['colorbox_caption_custom'], array('node' => $node));
-      break;
-    default:
-      $caption = '';
-  }
-
-  // Shorten the caption for the example styles or when caption shortening is active.
-  $colorbox_style = variable_get('colorbox_style', 'default');
-  $trim_length = variable_get('colorbox_caption_trim_length', 75);
-  if (((strpos($colorbox_style, 'colorbox/example') !== FALSE) || variable_get('colorbox_caption_trim', 0)) && (drupal_strlen($caption) > $trim_length)) {
-    $caption = drupal_substr($caption, 0, $trim_length - 5) . '...';
-  }
-
-  // Build the gallery id.
-  $nid = !empty($node->nid) ? $node->nid : 'nid';
-  switch ($settings['colorbox_gallery']) {
-    case 'post':
-      $gallery_id = 'gallery-' . $nid;
-      break;
-    case 'page':
-      $gallery_id = 'gallery-all';
-      break;
-    case 'field_post':
-      $gallery_id = 'gallery-' . $nid . '-' . $field['field_name'];
-      break;
-    case 'field_page':
-      $gallery_id = 'gallery-' . $field['field_name'];
-      break;
-    case 'custom':
-      $gallery_id = $settings['colorbox_gallery_custom'];
-      break;
-    default:
-      $gallery_id = '';
-  }
-
-  //This is our overrides. In every photo share node set the link to own node page content, and because of it's rendered
-  //via views, need to set $gallery_id to use same rel attribute in colorbox link.
-  //This is a little bit hacky, but the the complete photo sharing system is a big-big hack now..
-  if ($node->type == 'photo_shared') {
-    $path = 'node/' . $node->nid . '/colorbox-photo';
-    $gallery_id = 'gallery-all';
-  }
-  else if ($style_name = $settings['colorbox_image_style']) {
-    $path = image_style_url($style_name, $image['path']);
-  }
-  else {
-    $path = file_create_url($image['path']);
-  }
-  dsm(get_defined_vars());
-  return theme('colorbox_imagefield', array('image' => $image, 'path' => $path, 'title' => $caption, 'gid' => $gallery_id));
 }
