3896dc6938609f648bfe9964063802df0cf6c224
Gabor Szanto <hello@szantogabor.com>
2011-11-20 23:20:15 +0100

    Typo fix

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -88,7 +88,7 @@
   <?php print $user_picture; ?>
 
   <?php print render($title_prefix); ?>
-  <?php if (!$page && !referencing_field  && $title): ?>
+  <?php if (!$page && !$referencing_field  && $title): ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
