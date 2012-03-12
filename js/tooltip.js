b45b78fc65a00bd5595fd9c3b8d04b2c08164a09
szantog <szantog@610310.no-reply.drupal.org>
2012-03-12 14:30:30 +0100

    Try to set default button on add photo-shared map chrome bug fix 17

--- js/tooltip.js
+++ js/tooltip.js
@@ -61,7 +61,7 @@
       //but CiviCRM doesn't add class to them.
      $("#birth_date_display", context).addClass('required');
 
-     $("div#OpenLayers\\.Control\\.EditingToolbar_26", context).addClass('test');
+     $("div#OpenLayers\\.Control\\.EditingToolbar_26 div:last", context).addClass('test');
     }
   };
 
